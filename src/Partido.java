66082fe172e60bfdac0fed596a68eaf172f615c0
Leandro Chelentano <chelentanoleandro@gmail.com>
2022-09-17 15:40:21 -0300

    First commit

---
+++ src/Partido.java
@@ -0,0 +1,11 @@
+import java.util.Date;
+
+public class Partido {
+    private int id;
+    private String estadio;
+    private Date fecha;
+    private int hora;
+    private String clima;
+    private int minutosJugados = -1;
+
+}
