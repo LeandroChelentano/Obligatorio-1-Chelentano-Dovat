612b781c963086f1fd393d7ce3131c12e615caa8
Leandro Chelentano <chelentanoleandro@gmail.com>
2022-09-17 16:42:32 -0300

    Create Controladora.java

---
+++ src/Controladora.java
@@ -0,0 +1,12 @@
+import edu.obligatorio.com.Jugador;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Controladora {
+    private static ArrayList<Jugador> listaJugadores = new ArrayList<Jugador>();
+    private static ArrayList<Jugador> listaDTs = new ArrayList<Jugador>();
+    private static ArrayList<Jugador> listaPartidos = new ArrayList<Jugador>();
+    private static ArrayList<Jugador> listaArbitros = new ArrayList<Jugador>();
+
+}
