8def74d62b8707246e074712b9f46298881476a4
Leandro Chelentano <chelentanoleandro@gmail.com>
2022-10-15 18:08:07 -0300

    Format

--- src/edu/obligatorio/com/classes/Arbitro.java
+++ src/edu/obligatorio/com/classes/Arbitro.java
@@ -1,34 +1,26 @@
 package edu.obligatorio.com.classes;
 
-
 public class Arbitro extends Persona {
-
-
     private String puesto;
 
-    // Geters y Seters
-
-
-    String getPuesto() {
+//    #region Getters y Setters
+    public String getPuesto() {
         return puesto;
     }
-
     public void setPuesto(String puesto) {
         this.puesto = puesto;
     }
+//    #endregion
 
     @Override
     public String toString() {
         return super.toString() + ", en el puesto " + puesto + ".";
     }
 
-    public Arbitro() {
-    }
+    public Arbitro() {}
 
     public Arbitro(short aId, String aName, String aSurname, String aPosition) {
         super(aId, aName, aSurname);
         puesto = aPosition;
     }
-
-
 }
